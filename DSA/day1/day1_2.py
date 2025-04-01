fd67470d6cab02ec7acb26977f2ae4789b183d1c
Yokeyntrra.S <137605923+Yokey-01@users.noreply.github.com>
2025-04-01 09:51:17 +0530

    Delete DSA/day1 directory

--- DSA/day1/day1_2.py
+++
@@ -1,7 +0,0 @@
-price=list(map(int,input().split()))
-max=0
-for i in range(len(price)):
-	for j in range(i+1,len(price)):
-		if price[i]>price[j] and max<price[i]-price[j]:
-			max=price[i]-price[j]
-print(max)
